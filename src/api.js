f73de8da2b88e4b83f18f1f446af947a1bc2567f
Nick Kardish <kardish515@gmail.com>
2018-04-27 11:15:14 -0700

    add functionality to return list of doctors upon doctor search

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ class ApiCall {
   getData(userInput, displayData) {
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${userInput}&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=26c08aa36ece8a43fcc46338c45a6f31`
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${userInput}&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
       request.onload = function() {
         if(this.status === 200) {
           resolve(request.response);
@@ -20,3 +20,5 @@ class ApiCall {
     });
   }
 }
+
+export {ApiCall};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import $ from 'jquery';
+import $ from 'jquery';
+import {ApiCall} from './api.js';
+
+$(document).ready(function(){
+  let apiCall = new ApiCall();
+
+  let displayData = function(results){
+    let body=JSON.parse(results);
+    $('#doctor-list').empty();
+    for(let key in body.data) {
+      $('#doctor-list').append('<li>' + body.data[key].profile.first_name + ' ' + body.data[key].profile.last_name + '</li>');
+    }
+  }
+
+  $('#doctor').submit(function(event) {
+    event.preventDefault();
+    let userInput = $('#user-input').val();
+    apiCall.getData(userInput, displayData);
+  });
+});
