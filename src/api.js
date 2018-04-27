b649c188049571f03edf7e98ee12825601f4b2d6
Nick Kardish <kardish515@gmail.com>
2018-04-27 14:12:37 -0700

    add functionality to search for doctor by medical issue

--- src/api.js
+++ src/api.js
@@ -19,6 +19,26 @@ class ApiCall {
       return `something went wrong: ${error.message}`;
     });
   }
+  getData2(userInput, displayData){
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${userInput}&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
+      request.onload = function() {
+        if(this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+    promise.then(function(results) {
+      displayData(results)
+    }, function(error) {
+      return `something went wrong: ${error.message}`;
+    });
+  }
 }
 
 export {ApiCall};
--- src/main.js
+++ src/main.js
@@ -12,7 +12,6 @@ $(document).ready(function(){
     $('#doctor-list').empty();
     for(let key in body.data) {
       $('#doctor-list').append('<li>' + body.data[key].profile.first_name + ' ' + body.data[key].profile.last_name + '</li>');
-      console.log(body.data[key].practices);
       $('#doctor-list').append('<li>' + body.data[key].practices[0].visit_address.street + '</li>');
       for (let i = 0; i < body.data[key].practices.length; i++) {
         if(body.data[key].practices[i].within_search_area === true){
@@ -24,6 +23,8 @@ $(document).ready(function(){
           }
           if(body.data[key].practices[i].website !== undefined){
             $('#doctor-list').append('<li>' + body.data[key].practices[i].website + '</li>');
+          } else {
+            $('#doctor-list').append('<li>No website available</li>');
           }
           if(body.data[key].practices[i].accepts_new_patients === true){
             $('#doctor-list').append('<li>Currently accepting new patients</li>');
@@ -41,5 +42,10 @@ $(document).ready(function(){
     event.preventDefault();
     let userInput = $('#user-input').val();
     apiCall.getData(userInput, displayData);
+  });
+  $('#medical-issue').submit(function(event) {
+    event.preventDefault();
+    let userInput2 = $('#user-input2').val();
+    apiCall.getData2(userInput2, displayData);
   });
 });
